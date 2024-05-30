875e127e03e4a4611fa1147e378cd3e560fcc72a
Heyner Javier Marmol <contacto@javymarmol.com>
2024-05-29 20:32:57 -0500

    3.7 Homework - sample solution

--- core/domain/src/test/java/com/ivy/core/domain/algorithm/calc/RawStatsTest.kt
+++ core/domain/src/test/java/com/ivy/core/domain/algorithm/calc/RawStatsTest.kt
@@ -1,13 +1,10 @@
 package com.ivy.core.domain.algorithm.calc
 
-import arrow.core.computations.result
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.ivy.core.domain.algorithm.calc.data.RawStats
 import com.ivy.core.persistence.algorithm.calc.CalcTrn
-import com.ivy.data.CurrencyCode
 import com.ivy.data.transaction.TransactionType
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.time.Instant
@@ -70,4 +67,41 @@ class RawStatsTest {
         val result = rawStats(trans)
         assertThat(result).isEqualTo(rawStats)
     }
+
+    @Test
+    fun `Test creating raw stats from transactions`() {
+        val tenSecondsAgo = Instant.now().minusSeconds(10)
+        val fiveSecondsAgo = Instant.now().minusSeconds(5)
+        val threeSecondsAgo = Instant.now().minusSeconds(5)
+        val stats = rawStats(
+            listOf(
+                CalcTrn(
+                    amount = 5.0,
+                    currency = "EUR",
+                    type = TransactionType.Income,
+                    time = tenSecondsAgo
+                ),
+                CalcTrn(
+                    amount = 3.0,
+                    currency = "USD",
+                    type = TransactionType.Expense,
+                    time = threeSecondsAgo
+                ),
+                CalcTrn(
+                    amount = 10.0,
+                    currency = "USD",
+                    type = TransactionType.Expense,
+                    time = fiveSecondsAgo
+                ),
+            )
+        )
+
+        assertThat(stats.expensesCount).isEqualTo(2)
+        assertThat(stats.newestTrnTime).isEqualTo(threeSecondsAgo)
+        assertThat(stats.expenses).isEqualTo(mapOf("USD" to 13.0))
+
+        assertThat(stats.incomesCount).isEqualTo(1)
+        assertThat(stats.incomes).isEqualTo(mapOf("EUR" to 5.0))
+    }
+
 }
